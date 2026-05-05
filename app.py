8f81e1507bc1d46eeb757367932de20ce9756cba
LuxCaspian <luxcaspian@gmail.com>
2026-05-05 18:59:26 +0700

    update app.py #9

--- app.py
+++ app.py
@@ -37,7 +37,6 @@ st.markdown("""
     h1.main-title {
         font-size: 2.1rem;
         font-weight: 700;
-        white-space: nowrap;
         background: -webkit-linear-gradient(45deg, #1db954, #1ed760);
         -webkit-background-clip: text;
         -webkit-text-fill-color: transparent;
